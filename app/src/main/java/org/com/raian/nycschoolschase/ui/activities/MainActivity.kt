fd59d37fd7581df3ceee16de7c0d87966cc7c7d0
joNA <jitzian@gmail.com>
2019-08-11 18:16:17 -0400

    fun jc - Refactoring Base and MainActivity for handling offline/online scnearios and fetching data

--- app/src/main/java/org/com/raian/nycschoolschase/ui/activities/MainActivity.kt
+++ app/src/main/java/org/com/raian/nycschoolschase/ui/activities/MainActivity.kt
@@ -8,7 +8,6 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
 import android.os.Looper
 import android.provider.Settings
@@ -36,19 +35,10 @@ class MainActivity : BaseActivity(), NetworkReceiver.NetworkListener {
         }
     }
 
-    @SuppressLint("RestrictedApi")
     override fun initViews() {
-        mLocationRequest = LocationRequest()
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            buildAlertMessageNoGps()
-        }
-
-        if (checkPermissionForLocation(this)) {
-            startLocationUpdates()
-        }
     }
 
+    @SuppressLint("RestrictedApi")
     override fun getNetworkStatus(isConnected: Boolean) {
         try {
             if (!isConnected) {
@@ -63,6 +53,14 @@ class MainActivity : BaseActivity(), NetworkReceiver.NetworkListener {
                     .add(R.id.mFrameLayoutSchoolsContainer, SchoolsFragment(), SchoolsFragment::class.java.simpleName)
                     .commit()
 
+                mLocationRequest = LocationRequest()
+                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    buildAlertMessageNoGps()
+                }
+                if (checkPermissionForLocation(this)) {
+                    startLocationUpdates()
+                }
             }
         } catch (rte: RuntimeException) {
             logger.severe("$TAG::getNetworkStatus::${rte.message}")
@@ -73,13 +71,13 @@ class MainActivity : BaseActivity(), NetworkReceiver.NetworkListener {
         val builder = AlertDialog.Builder(this)
         builder.setMessage(resources.getString(R.string.message_gps_not_enabled))
             .setCancelable(false)
-            .setPositiveButton(resources.getString(R.string.message_Yes)) { _, _ ->
+            .setPositiveButton(resources.getString(R.string.message_yes)) { _, _ ->
                 startActivityForResult(
                     Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
                     , 11
                 )
             }
-            .setNegativeButton("No") { dialog, id ->
+            .setNegativeButton(resources.getString(R.string.message_no)) { dialog, _ ->
                 dialog.cancel()
                 finish()
             }
@@ -90,10 +88,9 @@ class MainActivity : BaseActivity(), NetworkReceiver.NetworkListener {
 
     private fun startLocationUpdates() {
         // Create the location request to start receiving updates
-
         mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-//        mLocationRequest.interval = INTERVAL
-//        mLocationRequest.fastestInterval = FASTEST_INTERVAL
+        mLocationRequest.interval = INTERVAL
+        mLocationRequest.fastestInterval = FASTEST_INTERVAL
 
         // Create LocationSettingsRequest object using location request
         val builder = LocationSettingsRequest.Builder()
@@ -126,50 +123,46 @@ class MainActivity : BaseActivity(), NetworkReceiver.NetworkListener {
             // do work here
             locationResult.lastLocation
             onLocationChanged(locationResult.lastLocation)
+
+
         }
     }
 
     fun onLocationChanged(location: Location) {
         // New location has now been determined
-
         mLastLocation = location
-        logger.severe("$TAG::onLocationChanged::${mLastLocation.latitude}")
-        logger.severe("$TAG::onLocationChanged::${mLastLocation.longitude}")
         stopLocationUpdates()
+
+        val bundle = Bundle()
+        bundle.putDouble("latitude", mLastLocation.latitude)
+        bundle.putDouble("longitude", mLastLocation.longitude)
+
+        val weatherFragment = WeatherFragment()
+        weatherFragment.arguments = bundle
+
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mFrameLayoutWeatherContainer, weatherFragment, WeatherFragment::class.java.simpleName)
+            .add(R.id.mFrameLayoutSchoolsContainer, SchoolsFragment(), SchoolsFragment::class.java.simpleName)
+            .commit()
+
     }
 
     private fun stopLocationUpdates() {
         mFusedLocationProviderClient?.removeLocationUpdates(mLocationCallback)
     }
 
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == REQUEST_PERMISSION_LOCATION) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationUpdates()
             } else {
-                Toast.makeText(this@MainActivity, "Permission Denied", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    resources.getString(R.string.message_permission_denied),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
-
-    private fun checkPermissionForLocation(context: Context): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (context.checkSelfPermission(ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                true
-            } else {
-                // Show the permission request
-                ActivityCompat.requestPermissions(
-                    this, arrayOf(ACCESS_FINE_LOCATION),
-                    REQUEST_PERMISSION_LOCATION
-                )
-                false
-            }
-        } else {
-            true
-        }
-    }
-
 }
--- app/src/main/java/org/com/raian/nycschoolschase/ui/base/BaseActivity.kt
+++ app/src/main/java/org/com/raian/nycschoolschase/ui/base/BaseActivity.kt
@@ -1,9 +1,14 @@
 package org.com.raian.nycschoolschase.ui.base
 
+import android.Manifest
+import android.content.Context
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.location.Location
 import android.net.ConnectivityManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.material.snackbar.Snackbar
@@ -52,6 +57,25 @@ abstract class BaseActivity : AppCompatActivity(), NetworkReceiver.NetworkListen
     override fun onDestroy() {
         super.onDestroy().also {
             viewModelStore.clear()
+        }
+    }
+
+    protected fun checkPermissionForLocation(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                true
+            } else {
+                // Show the permission request
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSION_LOCATION
+                )
+                false
+            }
+        } else {
+            true
         }
     }
 }
